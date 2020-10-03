f24afe50f3a33b1ef74ff95b4c315b09d423da0a
adriano <adrianogaugusto@gmail.com>
2020-10-03 02:00:39 -0300

    Update launch.json, index.js, and 318 more files...

---
+++ routes/userRoute.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const { join } = require('path')
+const filePath = join(__dirname, 'users.json')
+
+const getUsers = () =>{
+    const data = fs.existsSync(filePath)
+    ? fs.readFileSync(filePath)
+    : []
+
+    try{
+        return JSON.parse(data)
+    } catch (error) {
+        return []
+    }
+}
+
+const saveUser = (users) => {
+    fs.writeFileSync(filePath, JSON.stringify(users, null, '\t'))
+}
+
+const userRoute = (app) => {
+    app.route('/users/:id?')
+    .get((req, res) => {
+        const users = getUsers()
+        res.send({users})
+    })
+    .post((req, res) => {
+        const users = getUsers()
+        if (users.filter(user => user.id === req.body.id ).length === 0) {
+            users.push(req.body)
+            saveUser(users)
+            res.status(201).send('OK')
+        } else {
+            res.status(202).send("ID já cadastrado.")
+        }
+        
+    })
+    .put((req, res) => {
+        const users = getUsers()
+        saveUser(users.map(user => {
+            if (user.id === req.params.id){
+                return {
+                    ...user,
+                    ...req.body
+                }
+            }
+            return user
+        }))
+        res.status(200).send('OK')
+    })
+    .delete((req, res) => {
+        const users = getUsers()
+        saveUser(users.filter(user => user.id !== req.params.id ))
+
+        res.status(200).send('OK')
+    })
+}
+
+module.exports = userRoute
